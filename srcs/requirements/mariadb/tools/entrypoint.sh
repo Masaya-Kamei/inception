3a2c1a45b8a497532f7247943df1cb42356d2896
mkamei <mkamei@student.42tokyo.jp>
2022-09-08 01:40:36 +0200

    Add wpuser@localhost

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -7,10 +7,12 @@ if [ -z "$(ls /var/lib/mysql)" ]; then
     do
         sleep 0.1
     done
-	mysql -u root <<- EOSQL
+	mariadb -u root <<- EOSQL
 		CREATE DATABASE $DB_WP_NAME;
-		CREATE USER '$DB_WP_USER_NAME'@'%' identified by '$DB_WP_USER_PASS';
-		GRANT ALL PRIVILEGES ON $DB_WP_NAME.* TO '$DB_WP_USER_NAME'@'%';
+		CREATE USER '$DB_WP_USER_NAME'@'%' IDENTIFIED BY '$DB_WP_USER_PASS';
+		GRANT ALL ON $DB_WP_NAME.* TO '$DB_WP_USER_NAME'@'%';
+		CREATE USER '$DB_WP_USER_NAME'@'localhost' IDENTIFIED BY '$DB_WP_USER_PASS';
+		GRANT ALL ON $DB_WP_NAME.* TO '$DB_WP_USER_NAME'@'localhost';
 		ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_USER_PASS';
 		FLUSH PRIVILEGES;
 	EOSQL
